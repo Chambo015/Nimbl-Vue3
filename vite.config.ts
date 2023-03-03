878b814204dd05283d414e87fe2356f384f1c45a
Eraly <81900112+Chambo015@users.noreply.github.com>
2023-03-03 16:51:04 +0600

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL,
+  base: "/Nimbl-Vue3/",
   plugins: [vue()],
   resolve: {
     alias: {
